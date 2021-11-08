f3c8a6ccfff7c3b5e1d61da2a8226c333fd06ca3
Fahersto <31221314+Fahersto@users.noreply.github.com>
2021-11-08 19:09:50 +0100

    commandline now uses processName instead of processId

--- pe_injection/dll_injection/classic_dll_injection.cpp
+++ pe_injection/dll_injection/classic_dll_injection.cpp
@@ -1,37 +1,71 @@
 #include <Windows.h>
 #include <string>
 #include <cstdio>
-
+#include <tlhelp32.h>
 
 /**
 * Injects a .dll file into a running process
-*
+* Supports 32- and 64 Bit applications.
 */
 int main(int argc, char* argv[])
 {
-	int processId;
+	char* processName;
 	char* dllPath;
 
 	if (argc != 3)
 	{
-		printf("Usage: *.exe processId absolutePath\n");
+		printf("Usage: *.exe processName absolutePath\n");
 		return 1;
 	}
 
-	processId = atoi(argv[1]);
+	processName = argv[1];
 	dllPath = argv[2];
 	
-	HANDLE processHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, processId);
-	if (!processHandle)
+	HANDLE processesSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS | TH32CS_SNAPTHREAD, 0);
+	if (processesSnapshot == INVALID_HANDLE_VALUE)
+	{
+		printf("Error %d - Failed to CreateToolhelp32Snapshot\n", GetLastError());
+		return 1;
+	}
+
+	// get first process of snapshot
+	PROCESSENTRY32 processEntry = { sizeof(PROCESSENTRY32) };
+	if (!Process32First(processesSnapshot, &processEntry))
+	{
+		printf("Error %d - Failed to Process32First\n", GetLastError());
+		return 1;
+	}
+
+	bool foundTargetProcess = false;
+	// iterate processes
+	do
+	{
+		// check if we found the target process
+		if (strcmpi(processEntry.szExeFile, processName) == 0)
+		{
+			foundTargetProcess = true;
+			break;
+		}
+	} while (Process32Next(processesSnapshot, &processEntry));
+
+	if (!foundTargetProcess)
+	{
+		printf("Error - Failed to find process: %s\n", processName);
+		return 1;
+	}
+
+	printf("[Info] - Found Process %s with id %d\n", processName, processEntry.th32ProcessID);
+
+	// acquire a handle to the target process
+	HANDLE targetProcessHandle = OpenProcess(PROCESS_ALL_ACCESS, 0, processEntry.th32ProcessID);
+	if (!targetProcessHandle)
 	{
 		printf("Error %d - Failed to aquire process handle\n", GetLastError());
 		return 1;
 	}
 
-	printf("[Info] - Acquired process handle %p\n", processHandle);
-
 	// allocate memory in target process
-	LPVOID remoteMemory = VirtualAllocEx(processHandle, nullptr, MAX_PATH, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
+	LPVOID remoteMemory = VirtualAllocEx(targetProcessHandle, nullptr, MAX_PATH, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
 	if (!remoteMemory)
 	{
 		printf("Error %d - Failed to allocate memory in target process\n", GetLastError());
@@ -41,13 +75,13 @@ int main(int argc, char* argv[])
 	printf("[Info] - Allocated remote memory at %p\n", remoteMemory);
 
 	// write path of .dll to target process
-	if (!WriteProcessMemory(processHandle, remoteMemory, dllPath, strlen(dllPath), nullptr))
+	if (!WriteProcessMemory(targetProcessHandle, remoteMemory, dllPath, strlen(dllPath), nullptr))
 	{
 		printf("Error %d - Failed to write .dll path to target process\n", GetLastError());
 		return 1;
 	}
 
-	printf("[Info] - Wrote dll path %s to target process %d \n", dllPath, processId);
+	printf("[Info] - Wrote dll path %s to target process %s \n", dllPath, processName);
 
 	// get handle to kernel32.dll (which is loaded by default)
 	HMODULE kernel32ModuleHandle = GetModuleHandle("kernel32.dll");
@@ -69,7 +103,7 @@ int main(int argc, char* argv[])
 
 	// create a thread in the target process which loads the .dll
 	HANDLE remoteThreadHandle = CreateRemoteThread(
-		processHandle,
+		targetProcessHandle,
 		nullptr,
 		NULL,
 		reinterpret_cast<LPTHREAD_START_ROUTINE>(kernel32LoadLibrary),
@@ -87,5 +121,5 @@ int main(int argc, char* argv[])
 	printf("[Info] - Created remote thread at %p\n", kernel32LoadLibrary);
 
 	CloseHandle(remoteThreadHandle);
-	CloseHandle(processHandle);
+	CloseHandle(targetProcessHandle);
 }
